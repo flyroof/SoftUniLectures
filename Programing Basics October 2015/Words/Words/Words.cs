10b73cda77653fa406f9c613d71bdfc04bc024eb
Spas Vutov <VutovSpas@gmail.com>
2015-12-08 21:16:43 +0200

    Added homework problem from programing basics october course;

---
+++ Programing Basics October 2015/Words/Words/Words.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Words
+{
+    class Words
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] words = input.Split(new[] {' ', ',', ';', '.'},
+                StringSplitOptions.RemoveEmptyEntries);
+
+            string longsetWord = string.Empty;
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i];
+                if (currentWord.Length > longsetWord.Length)
+                {
+                    longsetWord = currentWord;
+                }
+            }
+
+            Console.WriteLine(longsetWord);
+        }
+    }
+}
