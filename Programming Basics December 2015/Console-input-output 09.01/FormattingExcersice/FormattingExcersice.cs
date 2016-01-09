1e6d0ac261186cab5f8ff5854f29f25987e6ef24
Spas Vutov <VutovSpas@gmail.com>
2016-01-09 14:03:57 +0200

    Console input/output excersice

---
+++ Programming Basics December 2015/Console-input-output 09.01/FormattingExcersice/FormattingExcersice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class FormattingExcersice
+{
+    static void Main(string[] args)
+    {
+        // Read the input from console
+        int firstNumber = int.Parse(Console.ReadLine());
+        double secondNumber = double.Parse(Console.ReadLine());
+        double thirdNumber = double.Parse(Console.ReadLine());
+
+        // Convert the number to binary
+        string binary = Convert.ToString(firstNumber, 2).PadLeft(10,'0');
+
+        // Output
+        Console.WriteLine("|{0,-10:X}|{1}|{2,10:0.##}|{3,-10:F3}|", firstNumber, binary, secondNumber, thirdNumber);
+    }
+}
